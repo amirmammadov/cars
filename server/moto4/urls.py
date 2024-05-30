f9820355431b39e416dbbc138e0bdb15498a133c
lale <liyeva820@gmail.com>
2024-05-30 13:27:34 -0700

    updated api schema

--- server/moto4/urls.py
+++ server/moto4/urls.py
@@ -32,9 +32,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.IsAuthenticated,),
-    patterns=[
-        path('companies/', include(api_urls)),
-    ],
+   
 )
 
 
